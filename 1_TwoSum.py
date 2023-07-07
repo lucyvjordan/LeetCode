aa2a2e3c6898e8925b95128307f3527d2e4bd753
Lucy <86916789+lucyvjordan@users.noreply.github.com>
2023-07-07 13:50:30 +0100

    1_TwoSum (Optimised)

--- 1_TwoSum.py
+++ 1_TwoSum.py
@@ -1,28 +1,37 @@
 # Prompt: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-nums = [3, 3, 4]
-target = 6
+import time
+starttime = time.time()
 
-def findSum(nums, target):
+nums = [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,11,12]
+target = 23
+
+def FindSum(nums, target):
     
     newnums = list(filter(lambda x: x < target, nums))
     # filters numbers to those less than the target to reduce runtime
 
     for i in range(len(newnums)):
-        # goes through every number less than the target
-        for j in range(i, len(newnums) - 1):
-            # goes through every other number less than the target after the first number (so that sums are not attempted multiple times)
-            if newnums[i] + newnums[j + 1] == target:
-                if newnums[i] == newnums[j + 1]:
-                    # if the two numbers are the same
-                    indexes = [index for index, value in enumerate(nums) if value == newnums[i]]
-                    # find the two indexes where the number occurs
-                    print(indexes[0], indexes[1])
-                    # only prints the first two in case the number occurs more than twice
-                else:
-                    print(nums.index(newnums[i]), nums.index(newnums[j + 1]))
-                    # if different numbers, print the indexes of the numbers where they occur in the original array
+        # goes through every number
+        remaining = target - newnums[i]
+        # finds the number that needs to be added to current number to make the target
+
+        if remaining == newnums[i]:
+            # if the number needed is the same as the current number
+            if newnums.count(remaining) > 1:
+                # if there are multiple instances of the number in the array
+                indexes = [index for index, value in enumerate(nums) if value == remaining]
+                # find the indexes where the number occurs
+                print(indexes[0], indexes[1])
                 return
-            
-findSum(nums, target)
+            continue
+
+        if remaining in newnums:
+            # if the number needed is in the array then these values are the solution
+            print(nums.index(newnums[i]), nums.index(remaining))
+            # prints the original index of these two numbers
+            return      
+              
+FindSum(nums, target)
+print(time.time() - starttime)
 
